11bfe011b500eed889c909bbc40a878a3accc397
juan9321 <juanfariasbr@gmail.com>
2025-11-20 09:30:00 -0300

    feat(login): aplicação Spring Boot e configuração base

---
+++ login/src/main/java/com/retrocore/juan/mod2/login/LoginApplication.java
@@ -0,0 +1,12 @@
+package com.retrocore.juan.mod2.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.retrocore.juan.mod2")
+public class LoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginApplication.class, args);
+    }
+}
